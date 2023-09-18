eef351db9c747fa968c972d1b8dfb137d9aad720
Maciej Szwast <60657751+M4NCHU@users.noreply.github.com>
2023-09-18 23:17:17 +0200

    deploy fix 7

--- frontend/src/util/functions.ts
+++
@@ -1,9 +0,0 @@
-export function shortenText(
-  text: string,
-  textLen: number
-  ): string {
-    if (text.length <= textLen) {
-      return text
-    }
-    return text.substring(0, textLen) + "..."
-}
